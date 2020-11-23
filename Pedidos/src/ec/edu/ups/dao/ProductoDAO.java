629c12cc19099ab62c65ab3f4a6dc45b104a3f89
plojam <52389262+plojam@users.noreply.github.com>
2020-11-23 18:04:29 -0500

    Comienzo modelo DAO de producto

---
+++ Pedidos/src/ec/edu/ups/dao/ProductoDAO.java
@@ -0,0 +1,7 @@
+package ec.edu.ups.dao;
+
+import ec.edu.ups.modelo.Producto;
+
+public interface ProductoDAO extends GenericDAO<Producto, Integer> {
+
+}
---
+++ Pedidos/src/ec/edu/ups/jdbc/JDBCProductoDAO.java
@@ -0,0 +1,7 @@
+package ec.edu.ups.jdbc;
+
+import ec.edu.ups.dao.ProductoDAO;
+
+public class JDBCProductoDAO extends JDBCGenericDAO<Producto, Integer> implements ProductoDAO {
+
+}
