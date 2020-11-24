17d0c805a416569731197f3561010e33e3901dbe
Robbyn07 <robbyn_reyes@hotmail.com>
2020-11-23 22:37:10 -0500

    Creacion de Index
    Se creo un archivo index.html base para el inicio de la pagina.

---
+++ Pedidos/src/ec/edu/ups/controlador/IniciarSesionController.java
@@ -0,0 +1,58 @@
+package ec.edu.ups.controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ec.edu.ups.dao.DAOFactory;
+import ec.edu.ups.dao.UsuarioDAO;
+import ec.edu.ups.modelo.Usuario;
+
+/**
+ * Servlet implementation class IniciarSesionController
+ */
+@WebServlet("/IniciarSesionController")
+public class IniciarSesionController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private UsuarioDAO usuarioDao;
+    private Usuario usuario;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IniciarSesionController() {
+    	usuarioDao = DAOFactory.getFactory().getUsuarioDAO();
+    	usuario = new Usuario();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String url = null;
+		try {
+			int id = Integer.valueOf(request.getParameter("id"));
+			usuario = usuarioDao.read(id);
+			request.setAttribute("persona", usuario);
+			url = "/JSPs/login.jsp";
+		} catch (Exception e) {
+			url = "/JSPs/error.jsp";
+		}
+		getServletContext().getRequestDispatcher(url).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
