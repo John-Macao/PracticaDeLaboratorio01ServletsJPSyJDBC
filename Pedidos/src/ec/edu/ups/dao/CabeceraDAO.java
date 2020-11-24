7df64cff2af33b4e176df0f24b9bb0276b67beec
plojam <52389262+plojam@users.noreply.github.com>
2020-11-23 23:25:41 -0500

    Arreglo modelo DAO

--- Pedidos/src/ec/edu/ups/dao/CabeceraDAO.java
+++ Pedidos/src/ec/edu/ups/dao/CabeceraDAO.java
@@ -1,7 +1,17 @@
 package ec.edu.ups.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import ec.edu.ups.modelo.Cabecera;
 
 public interface CabeceraDAO extends GenericDAO<Cabecera, Integer> {
-
+	
+	public abstract void crear(Cabecera cabecera, int usuarioId);
+	public abstract List<Cabecera> listarPorUsuario(int usuarioId);
+	public abstract int ultimoCreado();
+	
 }
--- Pedidos/src/ec/edu/ups/dao/ProductoDAO.java
+++ Pedidos/src/ec/edu/ups/dao/ProductoDAO.java
@@ -1,7 +1,14 @@
 package ec.edu.ups.dao;
 
+import java.util.List;
+
 import ec.edu.ups.modelo.Producto;
 
 public interface ProductoDAO extends GenericDAO<Producto, Integer> {
-
+	
+	public abstract void crear(Producto producto, int empresaId, int categoriaId);
+	public abstract int empresaId(int id);
+	public abstract int categoriaId(int id);
+	public abstract List<Producto> find(int empId);
+	
 }
